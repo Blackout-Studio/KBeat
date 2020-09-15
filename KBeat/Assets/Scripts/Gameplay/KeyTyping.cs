e6a376a855fa0477668903ae638b9af9dadec6d1
StarzzDrak <47496309+StarzzDrak@users.noreply.github.com>
2020-09-15 22:03:42 +0200

    15/09/20 #2
    KeyTyping progress

--- KBeat/Assets/Scripts/Gameplay/KeyTyping.cs
+++ KBeat/Assets/Scripts/Gameplay/KeyTyping.cs
@@ -1,17 +1,33 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 //Autor - Milan Savickij
 public class KeyTyping : MonoBehaviour
 {
-    void Start()
+
+    private int[] values;
+    private bool[] keys;
+
+    void Awake()
     {
-        
+        values = (int[])System.Enum.GetValues(typeof(KeyCode));
+        keys = new bool[values.Length];
     }
 
     void Update()
     {
-        
+        if (Input.anyKeyDown()) {
+            for (int i = 0; i < values.Length; i++)
+            {
+                keys[i] = Input.GetKey((KeyCode)values[i]);
+            }
+        }
+    }
+
+    void checkForKey() 
+    {
+    
     }
 }
