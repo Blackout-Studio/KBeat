b61b62f1340a943b3fdd0cd9fd0b41c2e85e202a
Matyas Himmer <35110087+M4TY@users.noreply.github.com>
2020-09-16 16:49:03 +0200

    16/9/2020 #1
    Added AudioManager, 4 new songs.

---
+++ KBeat/Assets/Scripts/Gameplay/AudioManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.Audio;
+using UnityEngine;
+using System;
+
+//Autor - Matyáš Himmer
+
+public class AudioManager : MonoBehaviour
+{
+
+    public Song[] songs;
+
+    void Awake()
+    {
+        foreach (Song s in songs)
+        {
+            s.source = gameObject.AddComponent<AudioSource>();
+            s.source.clip = s.clip;
+            s.source.volume = s.volume;
+            s.source.pitch = s.pitch;
+        }
+    }
+
+    public void Play (string name)
+    {
+        Song s = Array.Find(songs, song => song.name == name);
+        s.source.Play();
+    }
+
+    void Start()
+    {
+        Play("Cradles");
+        
+    }
+}
