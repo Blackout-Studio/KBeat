ab806fa1e07d48420789f7c550be83278609f9e1
sebi210dev <59232422+sebi210dev@users.noreply.github.com>
2020-09-18 21:34:56 +0200

    18.9.2020
    Implemented Credits Screen, soon to be fixed.

--- KBeat/Assets/Scripts/UI/CreditsScreenHandler.cs
+++ KBeat/Assets/Scripts/UI/CreditsScreenHandler.cs
@@ -2,13 +2,28 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using System.IO;
 
 public class CreditsScreenHandler : MonoBehaviour
 {
     public Text CreditsText;
     public TextAsset CreditsFile;
+    public Image BlackImage;
+    public GameObject MainMenu;
+    public GameObject CreditsFrame;
+
     public bool Sliding = false;
-    float speed = 0.8f;
+    float speed = 0.8f; 
+
+    public void FadeIn()
+    {
+        StartCoroutine(FadeImage(true));
+    }
+
+    public void FadeOut()
+    {
+        StartCoroutine(FadeOutImage(true));
+    }
 
     // Start is called before the first frame update
     void Start()
@@ -17,14 +32,17 @@ public class CreditsScreenHandler : MonoBehaviour
         //Credits += "hey\n";
         //Credits += "lool\n";
         //CreditsText.text = Credits;
-        CreditsText.text = CreditsFile.text;
+        string word = File.ReadAllText("Assets/Texts/Credits.txt");
+        CreditsText.text = word;
         StartSliding();
     }
 
     void StartSliding()
     {
         Sliding = true;
-
+        BlackImage.color = new Color(0, 0, 0, 0);
+        BlackImage.enabled = true;
+        FadeIn();
     }
 
     // Update is called once per frame
@@ -35,5 +53,45 @@ public class CreditsScreenHandler : MonoBehaviour
         Vector3 pos = CreditsText.transform.position;
         pos.y += speed;
         CreditsText.transform.position = pos;
+        
+        if (CreditsText.transform.position.y > 2500)
+        {
+            Sliding = false;
+            FadeOut();
+        }
+    }
+
+    //Fade Function
+    IEnumerator FadeImage(bool FadeAway)
+    {
+        Debug.Log("Start");
+        yield return new WaitForSeconds(0.5f);
+        if (FadeAway)
+        {
+            for (float i = 0; i <= 3; i += Time.deltaTime)
+            {
+                Debug.Log(i);
+                BlackImage.color = new Color(0, 0, 0, i);
+                yield return new WaitForSeconds(0.009f);
+            }
+            Debug.Log("Finished");            
+        }
+    }
+
+    IEnumerator FadeOutImage(bool FadeAway)
+    {
+        yield return new WaitForSeconds(1.5f);
+        if (FadeAway)
+        {
+            
+            for (float i = 1; i >= 0; i -= Time.deltaTime)
+            {
+                BlackImage.color = new Color(0, 0, 0, i);
+                yield return new WaitForSeconds(0.009f);
+            }
+            BlackImage.enabled = false;
+            CreditsFrame.SetActive(false);
+            CreditsText.transform.position = new Vector3(2.4f, -140.7f, 0);
+        }
     }
 }
