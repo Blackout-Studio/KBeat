3a2a461c056fc6190b0c01cf65504eebd699c235
Vojtysek <ojta@post.cz>
2020-09-23 21:58:53 +0200

    29.09.2020
    Back to LvlMenu a MainMenu - Vojta

---
+++ KBeat/Assets/GoToMenu.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GoToMenu : MonoBehaviour
+{
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Escape))
+        {
+            SceneManager.LoadScene("Menu");
+        }
+    }
+}
