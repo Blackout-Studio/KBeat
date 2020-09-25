05868ab4081eb47cf922f8097726c1aa2a207517
StarzzDrak <47496309+StarzzDrak@users.noreply.github.com>
2020-09-25 14:25:56 +0200

    25/09/20

--- KBeat/Assets/Scripts/Gameplay/Note.cs
+++ KBeat/Assets/Scripts/Gameplay/Note.cs
@@ -10,11 +10,14 @@ public class Note : MonoBehaviour
     private float speed;
     public KeyPressEvent KPE;
 
-    public void Initialize(double timeToHit, KeyCode kcode, int reactionTime)
+    public void Initialize(double timeToHit, KeyCode kcode, int reactionTime, Sprite image)
     {
         this.timeLeft = timeToHit*reactionTime;
         this.key = kcode;
         speed = (float)Mathf.Abs(this.gameObject.transform.position.z - 4)/(float)this.timeLeft;
+        GameObject child = this.gameObject.transform.GetChild(0).gameObject;
+        Renderer rend = child.GetComponent<Renderer>();
+        rend.material.mainTexture = image.texture;
     }
     
     void Update()
--- KBeat/Assets/Scripts/Gameplay/StartSpawning.cs
+++ KBeat/Assets/Scripts/Gameplay/StartSpawning.cs
@@ -10,13 +10,18 @@ public class StartSpawning : MonoBehaviour
 
     public KeyPressEvent kpe;
     public double cooldown;
-    private char[] c = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
+    private KeyCode[] letters = {KeyCode.A, KeyCode.B, KeyCode.C, KeyCode.D, KeyCode.E, KeyCode.F, KeyCode.G, KeyCode.H,
+                                 KeyCode.I, KeyCode.J, KeyCode.K, KeyCode.L, KeyCode.M, KeyCode.N, KeyCode.O, KeyCode.P,
+                                 KeyCode.Q, KeyCode.R, KeyCode.S, KeyCode.T, KeyCode.U, KeyCode.V, KeyCode.W, KeyCode.X, 
+                                 KeyCode.Y, KeyCode.Z};
+    [SerializeField]
+    private Sprite[] sprites;
     public void spawnOnBeat(int reactionTime)
     {
-        int key = Random.Range(0, c.Length);
+        int key = Random.Range(0, letters.Length);
         //KeyCode kc = (KeyCode)System.Enum.Parse(typeof(KeyCode), c[key].ToString());
-        Note n = Instantiate(note,new Vector3(4,-1,-16), Quaternion.EulerAngles(90,0,0)).GetComponent<Note>();
-        n.Initialize(cooldown, KeyCode.A, reactionTime);
+        Note n = Instantiate(note,new Vector3(4,-1,-16), Quaternion.EulerAngles(-90f,0f,0f)).GetComponent<Note>();
+        n.Initialize(cooldown, letters[key], reactionTime, sprites[key]);
         kpe.addNote(n);
     }
 
